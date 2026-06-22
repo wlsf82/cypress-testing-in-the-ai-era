859e5dc856c42d68b642ab234b95e1c2142abe7b
Walmyr <wlsf82@gmail.com>
2026-06-22 23:03:33 +0200

    Write e2e tests using the cypress-author skill

--- tat-csc/cypress.config.js
+++ tat-csc/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "https://tat-csc.s3.sa-east-1.amazonaws.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
