12c781444e249df4de0e0a0f9efcb00adc8b8da6
Walmyr <wlsf82@gmail.com>
2026-06-23 17:04:37 +0200

    Configure Cypress component testing for EmojiMart

--- emoji-mart/cypress.config.ts
+++ emoji-mart/cypress.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
+
   e2e: {
     baseUrl: "http://localhost:5173",
     fixturesFolder: false,
     supportFile: false,
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
   },
 });
